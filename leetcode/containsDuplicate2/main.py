6ff72cc383481c9033245b7a03b6306fafe39f1c
李俊杰 <ljj0475@175game.com>
2015-07-02 23:02:50 +0800

    containDuplicate2

---
+++ leetcode/containsDuplicate2/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {boolean}
+    def containsNearbyDuplicate(self, nums, k):
+        if nums is None:
+            return False
+        temp = {}
+        for i in xrange(len(nums)):
+            if temp.get(nums[i]) is None:
+                temp[nums[i]] = i
+            else:
+                if i - temp[nums[i]] <= k:
+                    return True
+                else:
+                    temp[nums[i]] = i
+        return False
+
+#nums = [0,1,2,3,0,4,5,6,7,8,9]
+#k = 9
+nums = [1]
+k = 1
+solution = Solution()
+print solution.containsNearbyDuplicate(nums,k)
