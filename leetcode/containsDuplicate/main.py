6be6050375d1fb7556a07b1c95ff0633c162d5d8
李俊杰 <mail@junjielee.com>
2015-07-02 23:22:12 +0800

    add containDuplicate

---
+++ leetcode/containsDuplicate/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {boolean)}
+    def containDuplicate(self, nums):
+        if nums is None:
+            return False
+        temp = dict()
+        for i in xrange(len(nums)):
+            if temp.get(nums[i]) is None:
+                temp[nums[i]] = i
+            else:
+                return True
+        return False
+
+nums = [1,1]
+solution = Solution()
+print solution.containDuplicate(nums)
