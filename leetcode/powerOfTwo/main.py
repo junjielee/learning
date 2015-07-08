6ccd560f2c31c3cda40c374e0fe9f15a2ceea2b6
李俊杰 <mail@junjielee.com>
2015-07-09 00:03:32 +0800

    add powerOfTwo

---
+++ leetcode/powerOfTwo/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class Solution:
+    # @param {integer} n
+    # @return {boolean}
+    def isPowerOfTwo(self, n):
+        return n > 0 and n & (n - 1) == 0
+
+
+test = range(21)
+test.append(100)
+solution = Solution()
+for i in test:
+    status = solution.isPowerOfTwo(i)
+    print '%s -> %s' % (i, status)
