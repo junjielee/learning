e64ca11d87265cd65db6b11d934f82b6b51f8219
李俊杰 <mail@junjielee.com>
2015-07-15 23:12:37 +0800

    delete ListNode

---
+++ leetcode/deleteNode/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    # @param {ListNode} node
+    # @return {void} Do not return anything, modify node in-place instead.
+    def deleteNode(self, node):
+        if node is None:
+            return
+        if node.next is not None:
+            node_next = node.next
+            node.val = node_next.val
+            node.next = node_next.next
+            del(node_next)
+        else:
+            node.val = None
+            del(node)
+
+
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+solution = Solution()
+
+
+def print_node_tree(root):
+    while root:
+        print root.val
+        root = root.next
+
+
+def test(node):
+    print_node_tree(node1)
+    solution.deleteNode(node)
+    print_node_tree(node1)
+
+# test(node1)
+# test(node2)
+test(node4)
