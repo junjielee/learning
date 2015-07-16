1a7dc8df484bdd89f5709cc891c24e58b15f5b59
李俊杰 <mail@junjielee.com>
2015-07-16 22:33:18 +0800

    add summary ranges

---
+++ leetcode/summaryRanges/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {string[]}
+    def summaryRanges(self, nums):
+        if not nums:
+            return []
+        string = []
+        begin = 0
+        for i in range(len(nums) - 1):
+            if nums[i] + 1 != nums[i + 1]:
+                string.append(self.__get_range(nums[begin], nums[i]))
+                begin = i + 1
+        string.append(self.__get_range(nums[begin], nums[len(nums) - 1]))
+        return string
+
+    def __get_range(self, begin, end):
+        if begin == end:
+            return str(begin)
+        else:
+            return '%s->%s' % (begin, end)
+
+
+test = [0, 1, 2, 4, 5, 7]
+test_one = [-1]
+solution = Solution()
+string = solution.summaryRanges(test_one)
+print string
