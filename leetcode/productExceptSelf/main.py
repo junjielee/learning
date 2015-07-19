fc2d55b477316ec823dfd73e78a1a0fe4a833bf8
李俊杰 <mail@junjielee.com>
2015-07-19 23:49:58 +0800

    add product except self

---
+++ leetcode/productExceptSelf/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[]}
+    def productExceptSelf(self, nums):
+        multiply_numbers = 1
+        zero_numbers = 0
+        for num in nums:
+            if num != 0:
+                multiply_numbers *= num
+            else:
+                zero_numbers += 1
+        if zero_numbers == 1:
+            return [0 if num else multiply_numbers for num in nums]
+        elif zero_numbers > 1:
+            return [0 for num in nums]
+        else:
+            return [multiply_numbers / num for num in nums]
+
+
+test = [1, 2, 3, 4]
+solution = Solution()
+products = solution.productExceptSelf(test)
+print products
