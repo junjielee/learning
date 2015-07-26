e38c5b1a641b4c25207577207c390844e214c870
李俊杰 <ljj0475@175game.com>
2015-07-27 07:59:17 +0800

    add happy number

---
+++ leetcode/happyNumber/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class Solution(object):
+    # @param {integer} n
+    # @return {boolean} n
+    def isHappy(self, n):
+        occured_nums = set()
+        while n != 1:
+            n = sum([pow(int(i), 2) for i in str(n)])
+            if n in occured_nums:
+                return False
+            else:
+                occured_nums.add(n)
+        return True
---
+++ leetcode/happyNumber/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from main import Solution
+
+num = 1
+solution = Solution()
+print solution.isHappy(num)
