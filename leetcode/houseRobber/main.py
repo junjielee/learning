b3df93ea4b67b7a95b8fed4f69042a5aa176b28a
李俊杰 <ljj0475@175game.com>
2015-08-06 23:27:23 +0800

    robber house,使用了动态规划

---
+++ leetcode/houseRobber/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class Solution(object):
+    # @param {integer[]} nums
+    # @return {integer}
+    # 时间复杂度O(n)，空间复杂度O(n)
+    def rob(self, nums):
+        size = len(nums)
+        max_before = [0] * (size + 1)
+        if size:
+            max_before[1] = nums[0]
+        for i in (range(2, size + 1)):
+            max_before[i] = max(max_before[i - 1], max_before[i - 2] + nums[i - 1])
+            print max_before
+        return max_before[size]
+
+    # 时间复杂度O(n)，空间复杂度O(1)
+    def rob_optimize(self, nums):
+        size = len(nums)
+        before, after = 0, 0
+        for i in range(size):
+            if i & 1:
+                before = max(before + nums[i], after)
+            else:
+                after = max(after + nums[i], before)
+        return max(before, after)
---
+++ leetcode/houseRobber/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from main import Solution
+
+
+solution = Solution()
+
+test = [9, 8, 6, 55, 34, 12]
+# test = [2, 1]
+print test
+
+result = solution.rob(test)
+print result
